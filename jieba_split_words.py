3cd928c6b6ab30d9915df46b3d64d37bdf205099
AmeliaLiu <244415578@qq.com>
2022-06-18 15:23:35 +0800

    jieba_split_words.py加入对搜狗字典的utf-8化处理，解除注释后可用

--- jieba_split_words.py
+++ jieba_split_words.py
@@ -1,21 +1,40 @@
 import jieba
 import os
+import chardet
+import codecs
+from django.utils.encoding import smart_text
 
 # TODO: 结合新词构建的词典，进一步加载，
 #  input: new_Dicts文件夹下的每个子文件夹（一个子文件夹代表一次迭代，一次迭代以天为单位）下的各个新词典
 
 
+def check_file_charset(file):
+    with open(file,'rb') as f:
+        return chardet.detect(f.read())
+
+# # 用于把搜狗词库转化为utf-8格式
+# dict_root_path = "./Dicts"
+# file_list = os.listdir(dict_root_path)
+# f_type = check_file_charset(os.path.join(dict_root_path, file_list[0]))
+# fin = open(os.path.join(dict_root_path, file_list[0]),'r',encoding=f_type['encoding'])
+# fout = open(os.path.join(dict_root_path, "sougou.txt"),'w',encoding='utf-8')
+# for line in fin:
+#     try:
+#         fout.write(line)
+#     except:
+#         pass
+# fin.close()
+# fout.close()
+
 def loadMyDicts():
     dict_root_path = "./Dicts"
     file_list = os.listdir(dict_root_path)
-    # TODO: sougou不是utf-8没有加入
     # TODO: 地名词典太大了，没有加入
     for file in file_list[1:]:
         # print(file)
         if file != "THUOCL_diming.txt":
             abs_path = os.path.join(dict_root_path, file)
             jieba.load_userdict(abs_path)
-
 
 
 # 对文件进行分词
@@ -43,6 +62,6 @@ def mySplitWords():
         fw = os.path.join(output_split_words_root_path, text_file)
         splitFile(fr,fw)
 
-if __name__ == '__main__':
-    loadMyDicts()
-    mySplitWords()
+# if __name__ == '__main__':
+    # loadMyDicts()
+    # mySplitWords()
